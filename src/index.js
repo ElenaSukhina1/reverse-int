d8e756947a5ca34ae3007de8961d60805bed36d5
ElenaSukhina1 <lenanikitina1@yandex.ru>
2023-01-05 14:44:32 +0300

    feat:task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  if (n <0){
+    n = n* (-1)
+  }
+  let str = String(n);
+  let revstr = '';
+  let newnumb;
+  for (let i = str.length - 1; i >= 0; i --){
+    revstr = revstr + str [i];
+  }
+  newnumb = parseInt(revstr)
+  return newnumb
 }
